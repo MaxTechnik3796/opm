e81e99cba1c2e408ffba8692d6d462eb2393fc8e
surik <suriken222a@gmail.com>
2026-05-11 16:51:18 +0200

    1.21.1 neoforge

---
+++ src/main/java/cz/maxtechnik/opm/mixin/MinecraftMixin.java
@@ -0,0 +1,15 @@
+package cz.maxtechnik.opm.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void opm$onInit(CallbackInfo ci) {
+        System.out.println("Mixins are active! Hello from OPM!");
+    }
+}
