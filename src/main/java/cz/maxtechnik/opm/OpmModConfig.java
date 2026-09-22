f416d73cedc92bc8ea0960b49718046a2104005e
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-09-22 15:58:28 +0200

    realms

--- src/main/java/cz/maxtechnik/opm/OpmModConfig.java
+++ src/main/java/cz/maxtechnik/opm/OpmModConfig.java
@@ -5,8 +5,12 @@ public class OpmModConfig{
 	public static final ModConfigSpec.Builder BUILDER=new ModConfigSpec.Builder();
 	public static final ModConfigSpec SPEC;
 	public static final ModConfigSpec.BooleanValue DEBUG;
+	public static final ModConfigSpec.BooleanValue NO_REALMS_BUTTON;
 	static{
 		DEBUG=BUILDER.define("debug",false);
+		BUILDER.push("general");
+		NO_REALMS_BUTTON=BUILDER.comment("Hides Realms button in main menu.").define("noRealmsButton",true);
+		BUILDER.pop();
 		SPEC=BUILDER.build();
 	}
 }
---
+++ src/main/java/cz/maxtechnik/opm/mixin/TitleScreenMixin.java
@@ -0,0 +1,37 @@
+package cz.maxtechnik.opm.mixin;
+
+import cz.maxtechnik.opm.OpmModConfig;
+import cz.maxtechnik.opm.OpmModTexts;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin{
+	@Inject(method="init", at=@At("TAIL"))
+	private void removeRealmsButton(CallbackInfo ci){
+		if(!OpmModConfig.NO_REALMS_BUTTON.get()) return;
+		TitleScreen self=(TitleScreen)(Object)this;
+		Button singleplayer=null;
+		Button multiplayer=null;
+		Button realms=null;
+		for(var widget: self.children()){
+			if(widget instanceof Button button){
+				switch(OpmModTexts.keyExtractor(button.getMessage().toString())){
+					case "menu.singleplayer" -> singleplayer=button;
+					case "menu.multiplayer" -> multiplayer=button;
+					case "menu.online" -> realms=button;
+				}
+			}
+		}
+		if(realms==null||singleplayer==null||multiplayer==null) return;
+		realms.visible=false;
+		realms.active=false;
+		int multiplayerY=multiplayer.getY();
+		int realmsY=realms.getY();
+		singleplayer.setY(multiplayerY);
+		multiplayer.setY(realmsY);
+	}
+}
