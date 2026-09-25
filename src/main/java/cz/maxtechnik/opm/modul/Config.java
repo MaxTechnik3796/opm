fc5a597c5c59bcb642a1856997ecd813a972d4ab
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-09-25 19:59:06 +0200

    scoreboard progress

--- src/main/java/cz/maxtechnik/opm/modul/Config.java
+++ src/main/java/cz/maxtechnik/opm/modul/Config.java
@@ -7,66 +7,84 @@ import cz.maxtechnik.opm.config.OpmModConfig;
 import cz.maxtechnik.opm.util.OpmButton;
 import cz.maxtechnik.opm.util.OpmColors;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFW;
 public class Config extends Screen{
-	Sidelist.ScoreboardBounds sidelistBounds;
-	OpmButton sidelistButton;
-	Button focusedButton=null;
-	boolean focusMode=false;
-	MousePos mousePos=new MousePos(0,0);
-	MousePos mouseOffset=new MousePos(0,0);
-	private record MousePos(int x,int y){
-	}
+	private Sidelist.ScoreboardBounds sidelistBounds;
+	private OpmButton sidelistButton;
+	private boolean focusMode=false;
+	private int grabOffsetX=0;
+	private int grabOffsetY=0;
 	public Config(){
 		super(Component.translatable("screen.opm.config"));
 	}
 	@Override
 	public void init(){
 		super.init();
-		if(focusMode){
-			OpmModConfig.SCOREBOARD_X.set(mousePos.x()-mouseOffset.x());
-			OpmModConfig.SCOREBOARD_Y.set(mousePos.y()-mouseOffset.y());
-			OpmModConfig.SPEC.save();
-		}
+		updateBoundsAndButton();
+	}
+	private void updateBoundsAndButton(){
 		sidelistBounds=Sidelist.getDummyBounds(font,width,height);
-		sidelistButton=new OpmButton(font,Component.empty(),sidelistBounds.x(),sidelistBounds.y(),sidelistBounds.width(),sidelistBounds.height(),button->setFocusedButton(button,new MousePos(mousePos.x()-button.getX(),mousePos.y()-button.getY())));
-		sidelistButton.setBackGroundColors(OpmColors.TRANSPARENT,OpmColors.BLUE,OpmColors.TRANSPARENT_WHITE);
-		addRenderableWidget(sidelistButton);
-	}
-	@Override
-	public void tick(){
-		if(focusMode) init();
-		super.tick();
-	}
-	@Override
-	public boolean isPauseScreen(){
-		return false;
+		if(sidelistButton==null){
+			sidelistButton=new OpmButton(font,Component.empty(),sidelistBounds.x(),sidelistBounds.y(),sidelistBounds.width(),sidelistBounds.height(),button->{});
+			sidelistButton.setBackGroundColors(OpmColors.TRANSPARENT,OpmColors.BLUE,OpmColors.TRANSPARENT_WHITE);
+			addRenderableWidget(sidelistButton);
+		}else{
+			sidelistButton.setX(sidelistBounds.x());
+			sidelistButton.setY(sidelistBounds.y());
+			sidelistButton.setWidth(sidelistBounds.width());
+			sidelistButton.setHeight(sidelistBounds.height());
+		}
 	}
 	@Override
 	public void render(@NotNull GuiGraphics gui,int mouseX,int mouseY,float partialTicks){
+		if(focusMode){
+			// 1. Požadovaný levý horní roh rámečku na obrazovce
+			int boxX=mouseX-grabOffsetX;
+			int boxY=mouseY-grabOffsetY;
+			// 2. Přepočet zpět na SCOREBOARD_X/Y s respektováním nastaveného Anchoru
+			int anchorOffsetX=switch(OpmModConfig.SCOREBOARD_ANCHOR_X.get()){
+				case RIGHT -> sidelistBounds.width();
+				case MIDDLE -> sidelistBounds.width()/2;
+				default -> 0;
+			};
+			int anchorOffsetY=switch(OpmModConfig.SCOREBOARD_ANCHOR_Y.get()){
+				case BOTTOM -> sidelistBounds.height();
+				case MIDDLE -> sidelistBounds.height()/2;
+				default -> 0;
+			};
+			OpmModConfig.SCOREBOARD_X.set(boxX+anchorOffsetX);
+			OpmModConfig.SCOREBOARD_Y.set(boxY+anchorOffsetY);
+			// Aktualizujeme pozici tlačítka bez volání init()
+			updateBoundsAndButton();
+		}
 		renderSidelist(gui);
 		sidelistButton.render(gui,mouseX,mouseY,partialTicks);
-		mousePos=new MousePos(mouseX,mouseY);
 	}
 	@Override
 	public boolean mouseClicked(double mouseX,double mouseY,int button){
+		if(button==0){
+			// Kliknuto do plochy sidebaru
+			if(sidelistButton.isMouseOver(mouseX,mouseY)){
+				focusMode=!focusMode;
+				if(focusMode){
+					// Chytnutí: přesně spočítáme offset od levého horního rohu rámečku
+					grabOffsetX=(int)mouseX-sidelistBounds.x();
+					grabOffsetY=(int)mouseY-sidelistBounds.y();
+				}else{
+					// Puštění: teprve teď uložíme změny na disk
+					OpmModConfig.SPEC.save();
+				}
+				return true;
+			}else if(focusMode){
+				focusMode=false;
+				OpmModConfig.SPEC.save();
+				return true;
+			}
+		}
 		return super.mouseClicked(mouseX,mouseY,button);
-	}
-	@Override
-	public boolean mouseDragged(double mouseX,double mouseY,int button,double dx,double dy){
-		return super.mouseDragged(mouseX,mouseY,button,dx,dy);
-	}
-	@Override
-	public boolean mouseReleased(double mouseX,double mouseY,int button){
-		return super.mouseReleased(mouseX,mouseY,button);
-	}
-	@Override
-	public boolean mouseScrolled(double mouseX,double mouseY,double scrollX,double scrollY){
-		return super.mouseScrolled(mouseX,mouseY,scrollX,scrollY);
 	}
 	@Override
 	public boolean keyPressed(int key,int scan,int mods){
@@ -74,49 +92,84 @@ public class Config extends Screen{
 			onClose();
 			return true;
 		}
-		switch(key){
-			case GLFW.GLFW_KEY_UP -> OpmModConfig.SCOREBOARD_Y.set(OpmModConfig.SCOREBOARD_Y.get()-1);
-			case GLFW.GLFW_KEY_DOWN -> OpmModConfig.SCOREBOARD_Y.set(OpmModConfig.SCOREBOARD_Y.get()+1);
-			case GLFW.GLFW_KEY_LEFT -> OpmModConfig.SCOREBOARD_X.set(OpmModConfig.SCOREBOARD_X.get()-1);
-			case GLFW.GLFW_KEY_RIGHT -> OpmModConfig.SCOREBOARD_X.set(OpmModConfig.SCOREBOARD_X.get()+1);
-			case GLFW.GLFW_KEY_KP_7 -> OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.LEFT);
-			case GLFW.GLFW_KEY_KP_8 -> OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.MIDDLE);
-			case GLFW.GLFW_KEY_KP_9 -> OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.RIGHT);
-			case GLFW.GLFW_KEY_KP_4 -> OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.TOP);
-			case GLFW.GLFW_KEY_KP_1 -> OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.MIDDLE);
-			case GLFW.GLFW_KEY_KP_0 -> OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.BOTTOM);
-			case GLFW.GLFW_KEY_SPACE -> OpmModConfig.SPEC.save();
-			default -> {
+		boolean changed=switch(key){
+			case GLFW.GLFW_KEY_UP -> {
+				OpmModConfig.SCOREBOARD_Y.set(OpmModConfig.SCOREBOARD_Y.get()-1);
+				yield true;
 			}
+			case GLFW.GLFW_KEY_DOWN -> {
+				OpmModConfig.SCOREBOARD_Y.set(OpmModConfig.SCOREBOARD_Y.get()+1);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_LEFT -> {
+				OpmModConfig.SCOREBOARD_X.set(OpmModConfig.SCOREBOARD_X.get()-1);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_RIGHT -> {
+				OpmModConfig.SCOREBOARD_X.set(OpmModConfig.SCOREBOARD_X.get()+1);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_7 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.LEFT);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_8 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.MIDDLE);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_9 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_X.set(AnchorX.RIGHT);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_4 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.TOP);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_1 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.MIDDLE);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_KP_0 -> {
+				OpmModConfig.SCOREBOARD_ANCHOR_Y.set(AnchorY.BOTTOM);
+				yield true;
+			}
+			case GLFW.GLFW_KEY_SPACE -> {
+				OpmModConfig.SPEC.save();
+				yield false;
+			}
+			default -> false;
+		};
+		if(changed){
+			updateBoundsAndButton();
 		}
-		init();
 		return super.keyPressed(key,scan,mods);
 	}
 	@Override
 	public void onClose(){
+		if(focusMode){
+			focusMode=false;
+			OpmModConfig.SPEC.save();
+		}
 		super.onClose();
 	}
-	private void setFocusedButton(Button button,MousePos offset){
-		if(focusMode) focusedButton=null;
-		else{
-			mouseOffset=offset;
-			focusedButton=button;
-		}
-		focusMode=!focusMode;
+	@Override
+	public boolean isPauseScreen(){
+		return false;
 	}
 	private void renderSidelist(GuiGraphics gui){
-		sidelistBounds=Sidelist.getDummyBounds(font,width,height);
 		int anchorX=1;
 		int anchorY=0;
 		switch(OpmModConfig.SCOREBOARD_ANCHOR_X.get()){
-			case RIGHT->anchorX=width-font.width("⚓");
-			case MIDDLE->anchorX=width/2-font.width("⚓")/2;
-			default -> {}
+			case RIGHT -> anchorX=width-font.width("⚓");
+			case MIDDLE -> anchorX=width/2-font.width("⚓")/2;
+			default -> {
+			}
 		}
 		switch(OpmModConfig.SCOREBOARD_ANCHOR_Y.get()){
-			case BOTTOM->anchorY=height-9;
-			case MIDDLE->anchorY=height/2-4;
-			default -> {}
+			case BOTTOM -> anchorY=height-9;
+			case MIDDLE -> anchorY=height/2-4;
+			default -> {
+			}
 		}
 		gui.drawString(font,"⚓",anchorX+2,anchorY+2,OpmColors.BLUE);
 		gui.renderOutline(anchorX,anchorY,font.width("⚓")+4,12,OpmColors.BLUE);
